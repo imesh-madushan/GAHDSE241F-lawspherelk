4bb3e8fd61d687a94654eddd14d67ddf4fd1a73d
imesh-madushan <imeshmadushan927@gmail.com>
2025-03-05 13:33:27 +0530

    sample serverjs created with node js backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +9,15 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+mongoose
+.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('🟢 Database connected!'))
+.catch((error) => console.log('🔴 Database connection failed!', error))
+
+app.get('/user', (req, res) => {
     res.send({ message: 'Hello from Node.js Backend!' });
 });
 
